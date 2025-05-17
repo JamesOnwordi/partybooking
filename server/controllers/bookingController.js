0a9796cfcdd9c08865ab8f059c61b56d4cf9ecd0
JamesOnwordi <jamesonwordi50@gmail.com>
2025-05-17 13:56:01 -0600

    setup mongoose connection and models boilerplate

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,36 +1,34 @@
-// const Bookings = require("../models/bookings")
-const asyncHandler = require("express-async-handler")
-
+// const Bookings = require("../models/booking")
+const asyncHandler = require('express-async-handler')
 
 // customer accessible routes
 // get available timeslots
 exports.booking_available = asyncHandler(async (req, res, next) => {
-    res.send(` list of available timeslot for a specific date `)
+  res.send(` list of available timeslot for a specific date `)
 })
 
-// create a new booking 
+// create a new booking
 exports.booking_create = asyncHandler(async (req, res, next) => {
-    res.send(`created a new booking`)
+  res.send(`created a new booking`)
 })
 
 // admin only routes
 // get all bookings
 exports.booking_list = asyncHandler(async (req, res, next) => {
-    res.send(` list of all bookings `)
+  res.send(` list of all bookings `)
 })
 
 // get specific date booking
-exports.booking_get = asyncHandler( async (req, res, next) => {
-    res.send( ` details for booking ${req.params.id}`)
+exports.booking_get = asyncHandler(async (req, res, next) => {
+  res.send(` details for booking ${req.params.id}`)
 })
 
-// edit a booking 
-exports.booking_edit = asyncHandler( async (req, res, next) => {
-    res.send(` edited booking ${req.params.id}`)
+// edit a booking
+exports.booking_edit = asyncHandler(async (req, res, next) => {
+  res.send(` edited booking ${req.params.id}`)
 })
 
-// delete a booking 
-exports.booking_delete =asyncHandler(async (req, res, next)=>{
-    res.send(` deleted booking ${req.params.id}`)
+// delete a booking
+exports.booking_delete = asyncHandler(async (req, res, next) => {
+  res.send(` deleted booking ${req.params.id}`)
 })
-
--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
-const bookingsRouter = require('./routes/bookings')
+require('dotenv').config()
+const bookingsRouter = require('./routes/booking')
 const adminRouter = require('./routes/admin')
+const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const port = 3000
 
+main().catch((err) => console.log(err))
+async function main() {
+  await mongoose.connect(process.env.MONGO_URI_DEV)
+  console.log(process.env.MONGO_URI_DEV, 'has connected succesfully ')
+}
 
 app.use('/admin', adminRouter)
-app.use('/',bookingsRouter)
+app.use('/', bookingsRouter)
 
 app.use(cors())
 
-app.listen( port, ()=>{
-    console.log(`Server running on port ${port}`)
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
 })
