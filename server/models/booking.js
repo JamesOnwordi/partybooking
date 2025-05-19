6a7ae04d4ef96ff46dfce76bef5002d6eacd4432
JamesOnwordi <jamesonwordi50@gmail.com>
2025-05-19 12:54:37 -0600

    boilerplate for booking schema

--- server/models/booking.js
+++ server/models/booking.js
@@ -2,6 +2,25 @@ const mongoose = require('mongoose')
 
 const { Schema } = mongoose
 
-const BookingSchema = new Schema({})
+const BookingSchema = new Schema({
+  date: { type: Date },
+  timeslot: [12, 2, 4, 6],
+  package: ['solarweek', 'solarweekend', 'galaxy'],
+  customer: {
+    name: { type: String },
+    phone: { type: Number },
+    email: { type: String }
+  },
+  celebrant: {
+    name: { type: String },
+    gender: { type: String },
+    turning: { type: Number }
+  },
+  capacity: {
+    kids: Number,
+    adults: Number
+  },
+  addons: [String]
+})
 
 // module.exports = mongoose.model('BookingSchema', BookingSchema)
