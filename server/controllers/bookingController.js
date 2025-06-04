56f3ac08b84bf1eb32bee3f2548c7ecd93642173
JamesOnwordi <jamesonwordi50@gmail.com>
2025-06-04 13:36:35 -0600

    adjusted logic for booking

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -2,30 +2,38 @@ const { raw } = require('express')
 const Bookings = require('../models/booking')
 const asyncHandler = require('express-async-handler')
 
+// constant value
+const MAX_ROOMS_PER_TIMESLOT = 2
+const TIMESLOTS = ['12PM', '2PM', '4PM', '6PM']
+
 // customer accessible routes
 // get available timeslots
+
 exports.timeslots_available = asyncHandler(async (req, res, next) => {
   try {
     const bookings = await Bookings.find(
       { date: req.params.date },
       'reservation.noOfRooms timeslot'
     ).exec()
-     const timeslots = ['12PM', '2PM', '4PM', '6PM']
-     const bookedTimeslot = bookings.map((booking) => {
-      console.log(booking)
-      if (bookedTimeslot[booking.bookedTimeslot]) {
-        bookedTimeslot[booking.bookedTimeslot] += booking.reservation.noOfRooms
-      } else {
-        bookedTimeslot[booking.bookedTimeslot] = booking.reservation.noOfRooms
-      }
+
+    const roomsBooked = {}
+    bookings.forEach(({ reservation: { noOfRooms }, timeslot }) => {
+      roomsBooked[timeslot] = (roomsBooked[timeslot] || 0) + noOfRooms
     })
-    res.status(201).json({
-      message: ' List of available timeslots',
-      bookedTimeslot
+
+    const availableRooms = {}
+    TIMESLOTS.forEach((slot) => {
+      availableRooms[slot] = MAX_ROOMS_PER_TIMESLOT - (roomsBooked[slot] || 0)
     })
-    console.log(` list of available timeslot for ${req.params.date} `)
+
+    res.status(200).json({
+      message: `List of available timeslots for ${req.params.date}`,
+      availableRooms
+    })
+
+    console.log(`List of available timeslots for ${req.params.date}`)
   } catch (error) {
-    console.error(' Error finding date')
+    console.error('Error finding date:', error)
     res.status(400).json({ error: error.message })
   }
 })
@@ -33,15 +41,25 @@ exports.timeslots_available = asyncHandler(async (req, res, next) => {
 // create a new booking
 exports.booking_create = asyncHandler(async (req, res, next) => {
   try {
-    if (bookingAvailable) {
-      const booking = await Bookings.create(req.body)
+    const bookingForm = req.body
+
+    if (
+      await bookingAvailable(
+        bookingForm.date,
+        bookingForm.timeslot,
+        bookingForm.reservation.noOfRooms
+      )
+    ) {
+      const createdBooking = await Bookings.create(bookingForm)
       res.status(201).json({
         message: 'Booking Created Succesfully',
-        booking
+        createdBooking
       })
+    } else {
+      res.status(400).json({ error: 'Exceeds room capacity' })
     }
   } catch (error) {
-    console.error('Booking creation error:', error.message)
+    console.error('Error creating booking:', error.message)
     res.status(400).json({ error: error.message })
   }
 })
@@ -50,32 +68,31 @@ exports.booking_create = asyncHandler(async (req, res, next) => {
 // get all bookings
 exports.booking_list = asyncHandler(async (req, res, next) => {
   try {
-    const booking = await Bookings.find({})
-    res.status(201).json({
+    const bookings = await Bookings.find({})
+    res.status(200).json({
       message: 'List of all bookings',
-      booking
+      bookings
     })
   } catch (error) {
-    console.error("Couln't access booking", error.message)
+    console.error('Error accessing all bookings:', error.message)
     res.status(400).json({ error: error.message })
   }
 })
 
-// // get a specific bookings
-// exports.booking_ = asyncHandler(async (req, res, next) => {
-//   res.send(` list of a specific booking ${req.params.id}`)
-// })
-
 // get specific date booking
 exports.booking_get = asyncHandler(async (req, res, next) => {
   try {
-    const booking = await Bookings.find({ date: req.params.id })
-    res.status(201).json({
-      message: `List of all ${req.params.id} bookings`,
-      booking
+    const bookings = await Bookings.find({ date: req.params.date })
+
+    res.status(200).json({
+      message: `List of bookings for ${req.params.date}`,
+      bookings
     })
   } catch (error) {
-    console.error("couldn't get booking", error.message)
+    console.error(
+      `Couldn't get bookings for ${req.params.date}:`,
+      error.message
+    )
     res.status(400).json({ error: error.message })
   }
 })
@@ -90,4 +107,16 @@ exports.booking_delete = asyncHandler(async (req, res, next) => {
   res.send(` deleted booking ${req.params.id}`)
 })
 
-const bookingAvailable = () => {}
+// check if timeslot choosen is available for booking
+const bookingAvailable = async (date, timeslot, noOfRooms) => {
+  const bookings = await Bookings.find(
+    { date: date, timeslot: timeslot },
+    'reservation.noOfRooms'
+  ).exec()
+
+  const roomBooked = bookings.reduce((total, booking) => {
+    return (total += booking.reservation.noOfRooms)
+  }, 0)
+
+  return MAX_ROOMS_PER_TIMESLOT - roomBooked >= noOfRooms ? true : false
+}
